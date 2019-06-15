fe62e25939b9ef751e0f138dcf7881329529e3ef
Crowdbotics <team@crowdbotics.com>
2019-06-15 17:59:05 +0000

    initial commit

---
+++ android/app/src/main/java/com/pocket_cinema_4713/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pocket_cinema_4713;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "pocket_cinema_4713";
+    }
+}
